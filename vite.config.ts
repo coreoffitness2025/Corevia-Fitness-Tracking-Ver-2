7baa9228b14a3e27fd9d054d2d10a8635024a0dc
Corevia <coreoffitness2025@gmail.com>
2025-05-01 15:35:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
   },
-  base: '/corevia-fitness-tracking/'
+  base: '/Corevia-Fitness-Tracking-Ver-2/' // 대소문자를 포함하여 정확히 저장소 이름과 일치해야 함
 });
