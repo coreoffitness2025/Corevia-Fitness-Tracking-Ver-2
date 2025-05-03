926e186899c108f385ee47fe24387be1677e8205
Corevia <coreoffitness2025@gmail.com>
2025-05-04 00:02:43 +0900

    Update BottomNavBar.tsx

--- src/components/common/BottomNavBar.tsx
+++ src/components/common/BottomNavBar.tsx
@@ -17,9 +17,18 @@ const BottomNavBar = () => {
           <span className="text-xs mt-1">홈</span>
         </Link>
         
-       <Link to="/record" className={`flex flex-col items-center ${isActive('/record')}`}>
+// 그래프 페이지 링크
+<Link to="/graph" className={`flex flex-col items-center ${isActive('/graph')}`}>
   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+  </svg>
+  <span className="text-xs mt-1">운동일지</span>
+</Link>
+
+// 운동 기록 페이지 링크 추가
+<Link to="/record" className={`flex flex-col items-center ${isActive('/record')}`}>
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
   </svg>
   <span className="text-xs mt-1">운동기록</span>
 </Link>
