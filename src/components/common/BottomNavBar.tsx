cb323ab09b47ff625d884ec649293936ee46c056
Corevia <coreoffitness2025@gmail.com>
2025-05-04 15:02:10 +0900

    Update BottomNavBar.tsx

--- src/components/common/BottomNavBar.tsx
+++ src/components/common/BottomNavBar.tsx
@@ -1,10 +1,9 @@
 import { Link, useLocation } from 'react-router-dom';
-
 const BottomNavBar = () => {
   const location = useLocation();
   
   const isActive = (path: string) => {
-    return location.pathname === path ? 'text-blue-500' : 'text-gray-500';
+    return location.pathname === path ? 'text-blue-500 dark:text-blue-400' : 'text-gray-500 dark:text-gray-400';
   };
   
   return (
@@ -22,6 +21,14 @@ const BottomNavBar = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
           </svg>
           <span className="text-xs mt-1">운동일지</span>
+        </Link>
+        
+        <Link to="/foodlog" className={`flex flex-col items-center ${isActive('/foodlog')}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+          <span className="text-xs mt-1">식단기록</span>
         </Link>
         
         <Link to="/qna" className={`flex flex-col items-center ${isActive('/qna')}`}>
@@ -42,5 +49,4 @@ const BottomNavBar = () => {
     </nav>
   );
 };
-
 export default BottomNavBar;
