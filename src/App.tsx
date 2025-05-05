159f51e6bf0890c8001f293babd439419d9ac851
Corevia <coreoffitness2025@gmail.com>
2025-05-05 16:45:01 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
-import WorkoutPage from './pages/WorkoutPage';
+import WorkoutPage from './pages/WorkoutPage'; // 운동 목록/입력 통합
 import WorkoutDetailPage from './pages/WorkoutDetailPage';
 import WorkoutNewPage from './pages/WorkoutNewPage';
 import WorkoutEditPage from './pages/WorkoutEditPage';
 import WorkoutStartPage from './pages/WorkoutStartPage';
 import WorkoutResultPage from './pages/WorkoutResultPage';
-import WorkoutRecordPage from './pages/WorkoutRecordPage';
-import FoodLogPage from './pages/FoodLogPage';
-import FoodRecordPage from './pages/FoodRecordPage';
+import WorkoutRecordPage from './pages/WorkoutRecordPage'; // 운동 기록 (운동 일지)
+import FoodLogPage from './pages/FoodLogPage'; // 식단 목록 
+import FoodRecordPage from './pages/FoodRecordPage'; // 식단 기록
 import QnaPage from './pages/QnaPage';
 import SettingPage from './pages/SettingPage';
 import SettingsPage from './pages/SettingsPage';
@@ -39,20 +39,32 @@ function App() {
       <ThemeProvider>
         <Layout>
           <Routes>
+            {/* Home */}
             <Route path="/" element={<HomePage />} />
+            
+            {/* Profile */}
             <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/workout" element={<WorkoutPage />} />
+            
+            {/* Workout Routes */}
+            <Route path="/workout" element={<WorkoutRecordPage />} /> {/* 운동 일지/기록 */}
             <Route path="/workout/:id" element={<WorkoutDetailPage />} />
             <Route path="/workout/new" element={<WorkoutNewPage />} />
             <Route path="/workout/:id/edit" element={<WorkoutEditPage />} />
             <Route path="/workout/:id/start" element={<WorkoutStartPage />} />
             <Route path="/workout/:id/result" element={<WorkoutResultPage />} />
-            <Route path="/workout/record" element={<WorkoutRecordPage />} />
-            <Route path="/food" element={<FoodLogPage />} />
-            <Route path="/food/record" element={<FoodRecordPage />} />
+            
+            {/* Food Routes */}
+            <Route path="/food" element={<FoodRecordPage />} /> {/* 식단 기록이 메인 */}
+            <Route path="/food/history" element={<FoodLogPage />} /> {/* 식단 기록 내역 */}
+            
+            {/* Q&A */}
             <Route path="/qna" element={<QnaPage />} />
+            
+            {/* Settings */}
             <Route path="/settings" element={<SettingsPage />} />
             <Route path="/settings/:tab" element={<SettingPage />} />
+            
+            {/* 404 Must be at the end */}
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Layout>
