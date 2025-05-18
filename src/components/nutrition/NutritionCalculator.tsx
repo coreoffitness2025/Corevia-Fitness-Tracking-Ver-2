d0e6503f524568badb9db826c6ac459f0872fc8d
coreoffitness2025 <coreoffitness2025@gmail.com>
2025-05-18 14:44:50 +0900

    불필요한 리다이렉션 파일(NutritionCalculator) 제거 및 코드 통합

--- src/components/nutrition/NutritionCalculator.tsx
+++
@@ -1,7 +0,0 @@
-import CalorieCalculator from './CalorieCalculator';
-
-// 호환성을 위한 리다이렉트 파일
-// CalorieCalculator로 리다이렉트하여 빌드 오류 해결
-const NutritionCalculator = CalorieCalculator;
-
-export default NutritionCalculator; 
