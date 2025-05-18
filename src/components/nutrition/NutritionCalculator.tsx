2680c0d22b98274e8229c34bbf9d6338c5134d8e
coreoffitness2025 <coreoffitness2025@gmail.com>
2025-05-18 14:43:07 +0900

    빌드 오류 수정: NutritionCalculator 리다이렉션 추가

---
+++ src/components/nutrition/NutritionCalculator.tsx
@@ -0,0 +1,7 @@
+import CalorieCalculator from './CalorieCalculator';
+
+// 호환성을 위한 리다이렉트 파일
+// CalorieCalculator로 리다이렉트하여 빌드 오류 해결
+const NutritionCalculator = CalorieCalculator;
+
+export default NutritionCalculator; 
